9cd88a2639af1eb41b1ea12167611e39e3185f93
Andrew <121983190+Andrew-laravel-php@users.noreply.github.com>
2024-07-17 01:05:08 +0300

    Create controller and route for CommentController
    Create controller and route for CommentController

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+
+class CommentController extends Controller
+{
+    public function index()
+    {
+        $comments = Comment::whereNull('parent_id')->with('replies')->paginate(25);
+        return response()->json($comments);
+    }
+    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_name' => 'required|regex:/^[a-zA-Z0-9]+$/',
+            'email' => 'required|email',
+            'home_page' => 'nullable|url',
+            'captcha' => 'required|captcha',
+            'text' => 'required|string',
+        ]);
+    
+        $comment = Comment::create($request->all());
+        return response()->json($comment);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CommentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('comments', [CommentController::class, 'index']);
+Route::post('comments', [CommentController::class, 'store']);
