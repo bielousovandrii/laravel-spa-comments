70e1555a6dc2b1fed00f7a8b7705acb750e2e580
Andrew <121983190+Andrew-laravel-php@users.noreply.github.com>
2024-07-17 01:22:19 +0300

    Add captcha to CommentForm
    Add captcha and refresh captcha func to vue page

--- resources/js/components/CommentForm.vue
+++ resources/js/components/CommentForm.vue
@@ -3,7 +3,10 @@
         <input v-model="form.user_name" placeholder="User Name" required />
         <input v-model="form.email" placeholder="Email" type="email" required />
         <input v-model="form.home_page" placeholder="Home Page" type="url" />
-        <input v-model="form.captcha" placeholder="CAPTCHA" required />
+        <div>
+            <img :src="captchaImage" @click="refreshCaptcha" />
+            <input v-model="form.captcha" placeholder="CAPTCHA" required />
+        </div>
         <textarea v-model="form.text" placeholder="Comment" required></textarea>
         <button type="submit">Submit</button>
     </form>
@@ -21,14 +24,24 @@ export default {
                 home_page: '',
                 captcha: '',
                 text: ''
-            }
+            },
+            captchaImage: ''
         };
     },
+    mounted() {
+        this.refreshCaptcha();
+    },
     methods: {
+        refreshCaptcha() {
+            axios.get('/captcha').then(response => {
+                this.captchaImage = response.data.captcha_src;
+            });
+        },
         submitComment() {
             axios.post('/api/comments', this.form).then(response => {
                 this.$emit('commentAdded', response.data);
                 this.resetForm();
+                this.refreshCaptcha();
             });
         },
         resetForm() {
