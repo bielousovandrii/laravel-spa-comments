ff47f31f1f7c86a4aa7e4aa75e6d5f1fb86abf76
Andrew <121983190+Andrew-laravel-php@users.noreply.github.com>
2024-07-17 01:35:52 +0300

    Fix Comment model
    Add protected $fillable's for comment's model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,13 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'user_name',
+        'email',
+        'home_page',
+        'text',
+        'parent_id',
+    ];
+
+    /**
+     * Get the replies for the comment.
+     */
     public function replies()
-        {
-            return $this->hasMany(Comment::class, 'parent_id');
-        }
-        
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
+
+    /**
+     * Get the parent comment.
+     */
     public function parent()
-        {
-            return $this->belongsTo(Comment::class, 'parent_id');
-        }
+    {
+        return $this->belongsTo(Comment::class, 'parent_id');
+    }
 }
