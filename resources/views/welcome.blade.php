bd721530ac65de0d6bf4216adf85f65d9d72437f
Andrew <121983190+Andrew-laravel-php@users.noreply.github.com>
2024-07-17 19:29:45 +0300

    Change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Comment System SPA v.a. (1.0.0)</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
