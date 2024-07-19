40e13859ce35d40336dec326afd7cc62ac8e20f4
Andrew <121983190+Andrew-laravel-php@users.noreply.github.com>
2024-07-19 23:41:12 +0300

    added middleware for api routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\CaptchaController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,6 +19,11 @@ use App\Http\Controllers\CaptchaController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('comments', [CommentController::class, 'index']);
-Route::post('comments', [CommentController::class, 'store']);
+
 Route::get('captcha', [CaptchaController::class, 'getCaptcha']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/comments', [CommentController::class, 'index']);
+    Route::post('/comments', [CommentController::class, 'store']);
+    Route::post('/comments/{comment}/replies', [CommentController::class, 'storeReply']);
+});
