f04bccdc1c9e9424d5e5ae7714116d8b38842827
Andrew <121983190+Andrew-laravel-php@users.noreply.github.com>
2024-07-22 02:35:00 +0300

    Create MessageSent.php

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class MessageSent implements ShouldBroadcast
+{
+    use InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('messages');
+    }
+
+    public function broadcastWith()
+    {
+        return ['message' => $this->message];
+    }
+}
