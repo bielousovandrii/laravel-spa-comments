af738048739075d130c658bbad79426089ce1e4d
Andrew <squirell1337@gmail.com>
2024-07-23 07:02:42 +0300

    kernel for pusher ws connection

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,21 +4,18 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
-    protected function schedule(Schedule $schedule): void
+    protected $commands = [
+        \App\Console\Commands\CreateCommentsIndex::class,
+    ];
+
+    protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
     }
 
-    /**
-     * Register the commands for the application.
-     */
-    protected function commands(): void
+    protected function commands()
     {
         $this->load(__DIR__.'/Commands');
 
