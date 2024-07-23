5ae83e816c25e328364db4d333a7835b24ac556e
Andrew <squirell1337@gmail.com>
2024-07-23 07:02:02 +0300

    add user table to comments table

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 //use Elasticsearch\ClientBuilder;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Comment extends Model
 {
@@ -16,6 +17,8 @@ class Comment extends Model
         'home_page',
         'text',
         'parent_id',
+        'photo',
+        'user_id'
     ];
 
     /**
@@ -35,7 +38,7 @@ class Comment extends Model
     }
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 //    protected static function boot()
 //    {
